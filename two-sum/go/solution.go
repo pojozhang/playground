e31588a431a2ff90a1dac7370de323c7cfbe19d6
moilions <baisong.zhang@yintech.cn>
2018-04-09 14:08:38 +0800

    add go run

--- two-sum/go/solution.go
+++ two-sum/go/solution.go
@@ -4,6 +4,36 @@ import (
 	"fmt"
 )
 
+func twoSum(nums []int, target int) []int {
+	for i := 0 ; i < len(nums) ; i ++ {
+		for j := i+ 1; j < len(nums) ; j ++ {
+			if nums[i] + nums[j] == target {
+				return []int{i , j}
+			}
+		}
+	}
+    return nums
+}
+
+
 func main() {
-	fmt.Print("111")
+
+	nums := []int{1,5,5,20}
+	target := 10
+
+	expect := []int{1,2}
+	answer := twoSum(nums , target)
+	result := true
+
+	for index, value := range expect {	
+		if answer[index] != value {
+			result = false
+		}
+	}
+
+	if result {
+		fmt.Println("Success")
+	} else {
+		fmt.Println("Oh no")
+	}
 }
