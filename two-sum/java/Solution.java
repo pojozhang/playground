ae4231e4697e5c367544113b0fa5aa3be5e94eca
moilions <baisong.zhang@yintech.cn>
2018-04-09 13:18:48 +0800

    add two sum java solution

--- two-sum/java/Solution.java
+++ two-sum/java/Solution.java
@@ -1,5 +1,30 @@
+import java.util.Arrays;
+
 public class Solution {
+    
     public int[] twoSum(int[] nums, int target) {
-        return new int[]{0};
+        for (int i = 0 , length = nums.length ; i < length ; i ++ ){
+            for (int j = i + 1 ; j < length ; j ++ ) {
+                if (nums[i] + nums[j] == target) 
+                    return new int[]{i , j};
+            }
+        }
+        return new int[]{0 , 0};
+    }
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        
+        int[] nums = new int[]{2,1,7,20};
+        int target = 9;
+
+        assert Arrays.equals(new int[]{0,2}, solution.twoSum(nums, target));
+        
+        nums = new int[]{1,5,5,20};
+        target = 10;
+
+        assert Arrays.equals(new int[]{1,1}, solution.twoSum(nums, target));
+        
+        System.out.println("congratulation!");
     }
 }
