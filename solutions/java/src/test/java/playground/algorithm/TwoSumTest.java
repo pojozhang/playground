93bea913d2b4853c3b5337b212213ab0197256df
POJO <pojo.zhang@gmail.com>
2018-04-12 00:01:22 +0800

    rename

--- solutions/java/src/test/java/playground/algorithm/TwoSumTest.java
+++ solutions/java/src/test/java/playground/algorithm/TwoSumTest.java
@@ -8,19 +8,19 @@ class TwoSumTest {
 
     @Test
     public void case_1() {
-        TwoSum twoSum = new TwoSum();
+        TwoSum solution = new TwoSum();
         int[] nums = new int[]{2, 1, 7, 20};
         int target = 9;
 
-        assertArrayEquals(new int[]{0, 2}, twoSum.twoSum(nums, target));
+        assertArrayEquals(new int[]{0, 2}, solution.twoSum(nums, target));
     }
 
     @Test
     public void case_2() {
-        TwoSum twoSum = new TwoSum();
+        TwoSum solution = new TwoSum();
         int[] nums = new int[]{1, 5, 5, 20};
         int target = 10;
 
-        assertArrayEquals(new int[]{1, 2}, twoSum.twoSum(nums, target));
+        assertArrayEquals(new int[]{1, 2}, solution.twoSum(nums, target));
     }
 }
