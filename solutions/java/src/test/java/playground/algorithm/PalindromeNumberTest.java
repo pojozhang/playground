3cf1394479964a7a03c8b1a5c186bdf127c68b3e
pojozhang <pojo.zhang@gmail.com>
2018-04-17 18:34:09 +0800

    clean

--- solutions/java/src/test/java/playground/algorithm/PalindromeNumberTest.java
+++ solutions/java/src/test/java/playground/algorithm/PalindromeNumberTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.math.BigDecimal;
-
 class PalindromeNumberTest {
 
     @Test
