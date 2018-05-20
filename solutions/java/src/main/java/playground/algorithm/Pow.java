7c27aabab59d81dc7386efce4f63ffe26a9cec1c
pojozhang <pojo.zhang@gmail.com>
2018-05-20 16:50:11 +0800

    update pow

--- solutions/java/src/main/java/playground/algorithm/Pow.java
+++ solutions/java/src/main/java/playground/algorithm/Pow.java
@@ -3,17 +3,9 @@ package playground.algorithm;
 public class Pow {
 
     public double myPow(double x, int n) {
-        if (x == 1) {
-            return 1;
-        }
-        if (x == -1) {
-            return n % 2 == 0 ? 1 : -1;
-        }
-        // Integer.MIN_VALUE 取反会溢出，因此要特殊处理
-        if (n == Integer.MIN_VALUE) {
-            return 0;
-        }
-        return n > 0 ? pow(x, n) : 1 / pow(x, -n);
+        //此处如果n<0不需要去反，如果取反会有溢出的可能（负数范围比正数大）
+        //如果取反，那么还要针对 n = Integer.MIN_VALUE 进行特殊处理，多此一举
+        return n >= 0 ? pow(x, n) : 1 / pow(x, n);
     }
 
     private double pow(double x, int n) {
@@ -25,7 +17,7 @@ public class Pow {
             return x;
         }
 
-        double value = myPow(x, n / 2);
+        double value = pow(x, n / 2);
 
         // 如果指数是偶数：x^2m = x^m * x^m
         // 如果指数是奇数：x^(2m+1) = x^m * x^m * x
