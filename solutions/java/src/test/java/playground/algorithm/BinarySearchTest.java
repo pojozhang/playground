8bfec0c68f3939040319149f555a9a2c87054928
pojozhang <pojo.zhang@gmail.com>
2018-05-22 17:56:53 +0800

    refactor

--- solutions/java/src/test/java/playground/algorithm/BinarySearchTest.java
+++ solutions/java/src/test/java/playground/algorithm/BinarySearchTest.java
@@ -1,24 +1,20 @@
 package playground.algorithm;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 class BinarySearchTest {
 
+    private BinarySearch solution = new BinarySearch();
+
     @Test
     void case_1() {
-        BinarySearch solution = new BinarySearch();
-        int[] nums = new int[]{0, 1, 2, 3, 4};
-
-        assertEquals(3, solution.search(nums, 3));
+        assertEquals(3, solution.search(new int[]{0, 1, 2, 3, 4}, 3));
     }
 
     @Test
     void case_2() {
-        BinarySearch solution = new BinarySearch();
-        int[] nums = new int[]{0, 1, 2, 3, 4};
-
-        assertEquals(-1, solution.search(nums, 6));
+        assertEquals(-1, solution.search(new int[]{0, 1, 2, 3, 4}, 6));
     }
 }
