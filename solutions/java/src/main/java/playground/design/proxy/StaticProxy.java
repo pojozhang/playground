6de9b9f142361e8ca3cd2d6512d16c40c14344e8
moilions <baisong.zhang@yintech.cn>
2018-05-28 11:29:08 +0800

    feat: static proxy

--- solutions/java/src/main/java/playground/design/proxy/StaticProxy.java
+++ solutions/java/src/main/java/playground/design/proxy/StaticProxy.java
@@ -1,11 +1,14 @@
 package playground.design.proxy;
 
 /**
- * 静态代理: 通过调用已知的对象方法，实现对原有的类的方法，进行扩展
+ * 静态代理: 被代理的对象为固定对象或者固定接口，可以通过hardcode的形式来完成代理模式编程，我们称这种代理方式为静态代理
+ *
+ * PS: 本文中的例子，是在当前代理类中创建了被代理对象，当然你也可以将被代理者通过参数传递进入代理类，但是本例子如要是侧重那些，被代理者是你能直接访问到的情况
  *
  * 场景选择: 已知类的行为，但是这个行为不能满足你现在的需求（春运反正我是很难抢到票），可以选择用代理模式进行扩展
  *
  * 优点: 对于拒绝修改，支持扩展原则，代理是一种很好的实现
+ * 缺点:
  */
 public class StaticProxy {
 
@@ -17,12 +20,12 @@ public class StaticProxy {
 
         @Override
         public void buyTickets() {
-            Customer customer = findWhoWantTickets();
+            RailWay customer = findWhoWantTickets();
             System.out.println("I Don't go home, But i can help u");
             customer.buyTickets();
         }
 
-        private Customer findWhoWantTickets() {
+        private RailWay findWhoWantTickets() {
             return new Customer();
         }
     }
