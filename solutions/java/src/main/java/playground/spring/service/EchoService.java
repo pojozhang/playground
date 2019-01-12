0b80b0fbe81283bbac8a7e45417d7dc2bdd40595
Tony <pojo.zhang@gmail.com>
2019-01-13 00:35:48 +0800

    bug fix

--- solutions/java/src/main/java/playground/spring/service/EchoService.java
+++ solutions/java/src/main/java/playground/spring/service/EchoService.java
@@ -3,11 +3,14 @@ package playground.spring.service;
 import org.springframework.stereotype.Service;
 import playground.spring.annotation.Log;
 
+import java.util.Objects;
+
 @Service
 public class EchoService {
 
     @Log
     public String echo(String string) {
+        Objects.requireNonNull(string);
         return string;
     }
 }
