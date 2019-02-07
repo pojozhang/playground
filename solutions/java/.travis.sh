2c96cdb469a94d2058705e665b89e470900dc2c4
Travis CI <travis@travis-ci.org>
2019-02-08 01:45:09 +0800

    fix

--- solutions/java/.travis.sh
+++ solutions/java/.travis.sh
@@ -1,7 +1,6 @@
-ll
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git clone git@github.com:pojozhang/playground-report.git
-git add solutions/java/build/reports/tests/test/*
+git add build/reports/tests/test/*
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 git push
