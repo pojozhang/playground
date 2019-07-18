8ae3f5a570f2b94ad82a93cb1cb13e4cc620e8ff
Tony <pojo.zhang@gmail.com>
2019-07-18 09:17:20 +0800

    refactor ListNode

--- solutions/java/src/main/java/playground/algorithm/common/ListNode.java
+++ solutions/java/src/main/java/playground/algorithm/common/ListNode.java
@@ -7,4 +7,29 @@ public class ListNode {
     public ListNode(int x) {
         val = x;
     }
+
+    public static ListNode of(int... values) {
+        ListNode head = new ListNode(values[0]);
+        ListNode tail = head;
+        for (int i = 1; i < values.length; i++) {
+            tail = tail.next = new ListNode(values[i]);
+        }
+        return head;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListNode listNode = (ListNode) o;
+        ListNode nextNode = this;
+        while (nextNode != null && listNode != null) {
+            if (nextNode.val != listNode.val) {
+                return false;
+            }
+            nextNode = nextNode.next;
+            listNode = listNode.next;
+        }
+        return nextNode == null && listNode == null;
+    }
 }
--- solutions/java/src/test/java/playground/algorithm/MergeTwoSortedListsTest.java
+++ solutions/java/src/test/java/playground/algorithm/MergeTwoSortedListsTest.java
@@ -4,28 +4,18 @@ import org.junit.jupiter.api.Test;
 import playground.algorithm.common.ListNode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 class MergeTwoSortedListsTest {
 
+    private MergeTwoSortedLists solution = new MergeTwoSortedLists();
+
     @Test
     void case_1() {
-        ListNode list1 = new ListNode(1);
-        list1.next = new ListNode(2);
-        list1.next.next = new ListNode(4);
+        ListNode list1 = ListNode.of(1, 2, 4);
+        ListNode list2 = ListNode.of(1, 3, 4);
 
-        ListNode list2 = new ListNode(1);
-        list2.next = new ListNode(3);
-        list2.next.next = new ListNode(4);
+        ListNode mergedList = solution.mergeTwoLists(list1, list2);
 
-        ListNode mergedList = new MergeTwoSortedLists().mergeTwoLists(list1, list2);
-
-        assertEquals(1, mergedList.val);
-        assertEquals(1, mergedList.next.val);
-        assertEquals(2, mergedList.next.next.val);
-        assertEquals(3, mergedList.next.next.next.val);
-        assertEquals(4, mergedList.next.next.next.next.val);
-        assertEquals(4, mergedList.next.next.next.next.next.val);
-        assertNull(mergedList.next.next.next.next.next.next);
+        assertEquals(ListNode.of(1, 1, 2, 3, 4, 4), mergedList);
     }
 }
