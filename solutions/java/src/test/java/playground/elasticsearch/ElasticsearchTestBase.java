7f4e315ac87758babb57062449f900374e9c793b
Tony <pojo.zhang@gmail.com>
2019-07-30 22:51:26 +0800

    add es tests

---
+++ solutions/java/src/test/java/playground/elasticsearch/ElasticsearchTestBase.java
@@ -0,0 +1,69 @@
+package playground.elasticsearch;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.support.WriteRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.reindex.DeleteByQueryRequest;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+
+import java.io.IOException;
+
+import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+abstract class ElasticsearchTestBase {
+
+    RestHighLevelClient client;
+
+    @BeforeAll
+    void init() throws IOException {
+        RestClientBuilder restClientBuilder = RestClient.builder(new HttpHost("localhost", 9200, "http"));
+        client = new RestHighLevelClient(restClientBuilder);
+    }
+
+    @AfterAll
+    void cleanUp() throws IOException {
+        client.close();
+    }
+
+    void createIndex(String index, String source) throws IOException {
+        CreateIndexRequest request = new CreateIndexRequest(index);
+        request.source(source, XContentType.JSON);
+        client.indices().create(request, RequestOptions.DEFAULT);
+    }
+
+    void deleteIndex(String index) throws IOException {
+        DeleteIndexRequest request = new DeleteIndexRequest(index);
+        client.indices().delete(request, RequestOptions.DEFAULT);
+    }
+
+    void cleanIndex(String index) throws IOException {
+        client.deleteByQuery(new DeleteByQueryRequest(index).setQuery(matchAllQuery()), RequestOptions.DEFAULT);
+    }
+
+    void index(IndexRequest indexRequest) throws IOException {
+        indexRequest.setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE);
+        client.index(indexRequest, RequestOptions.DEFAULT);
+    }
+
+    SearchResponse query(QueryBuilder queryBuilder) throws IOException {
+        SearchRequest searchRequest = new SearchRequest();
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.query(queryBuilder);
+        searchRequest.source(builder);
+        return client.search(searchRequest, RequestOptions.DEFAULT);
+    }
+}
---
+++ solutions/java/src/test/java/playground/elasticsearch/QueryTest.java
@@ -0,0 +1,75 @@
+package playground.elasticsearch;
+
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.elasticsearch.index.query.QueryBuilders.termQuery;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class QueryTest extends ElasticsearchTestBase {
+
+    private static final String INDEX = "query_test_index";
+
+    @Override
+    @BeforeAll
+    void init() throws IOException {
+        super.init();
+        @Language("JSON")
+        String source = "{\n" +
+                "  \"mappings\": {\n" +
+                "    \"properties\": {\n" +
+                "      \"text_field\": {\n" +
+                "        \"type\": \"text\",\n" +
+                "        \"analyzer\": \"standard\"\n" +
+                "      },\n" +
+                "      \"keyword_field\": {\n" +
+                "        \"type\": \"keyword\"\n" +
+                "      }\n" +
+                "    }\n" +
+                "  " +
+                "}\n" +
+                "}";
+        createIndex(INDEX, source);
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        cleanIndex(INDEX);
+    }
+
+    @Override
+    @AfterAll
+    void cleanUp() throws IOException {
+        deleteIndex(INDEX);
+        super.cleanUp();
+    }
+
+    @Test
+    void term_query_should_find_documents_that_contain_the_exact_term_in_a_field() throws IOException {
+        IndexRequest indexRequest = new IndexRequest(INDEX)
+                .source("keyword_field", "hello world");
+        index(indexRequest);
+
+        SearchResponse response = query(termQuery("keyword_field", "hello world"));
+
+        assertEquals(1, response.getHits().getTotalHits().value);
+    }
+
+    @Test
+    void term_query_should_not_find_documents_that_not_contain_the_exact_term_in_a_analyzed_field() throws IOException {
+        IndexRequest indexRequest = new IndexRequest(INDEX)
+                .source("text_field", "hello world");
+        index(indexRequest);
+
+        SearchResponse response = query(termQuery("text_field", "hello world"));
+
+        assertEquals(0, response.getHits().getTotalHits().value);
+    }
+}
