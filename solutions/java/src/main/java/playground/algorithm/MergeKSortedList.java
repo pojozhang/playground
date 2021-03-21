fb484b5c5295125a035f3a58b7530dec46294f24
Tony <pojo.zhang@gmail.com>
2021-03-21 21:51:24 +0800

    add MergeKSortedList

--- solutions/java/src/main/java/playground/algorithm/MergeKSortedList.java
+++ solutions/java/src/main/java/playground/algorithm/MergeKSortedList.java
@@ -1,2 +1,29 @@
-package playground.algorithm;public class MergeKSortedList {
+package playground.algorithm;
+
+import playground.algorithm.common.ListNode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class MergeKSortedList {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode sentinel = new ListNode(-1);
+        ListNode next = sentinel;
+        PriorityQueue<ListNode> heap = new PriorityQueue<>(Comparator.comparingInt(l -> l.val));
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] != null) {
+                heap.offer(lists[i]);
+            }
+        }
+        while (!heap.isEmpty()) {
+            ListNode peek = heap.poll();
+            next = next.next = peek;
+            if (peek.next != null) {
+                heap.offer(peek.next);
+            }
+        }
+        next.next = null;
+        return sentinel.next;
+    }
 }
--- solutions/java/src/test/java/playground/algorithm/MergeKSortedListTest.java
+++ solutions/java/src/test/java/playground/algorithm/MergeKSortedListTest.java
@@ -1,7 +1,35 @@
+package playground.algorithm;
 
 import org.junit.jupiter.api.Test;
+import playground.algorithm.common.ListNode;
+
+import java.lang.management.LockInfo;
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 class MergeKSortedListTest {
-  
+
+    private MergeKSortedList solution = new MergeKSortedList();
+
+    @Test
+    void case_1() {
+        ListNode sortedList = solution.mergeKLists(new ListNode[]{ListNode.of(1, 4, 5), ListNode.of(1, 3, 4), ListNode.of(2, 6)});
+
+        assertThat(sortedList).isEqualTo(ListNode.of(1, 1, 2, 3, 4, 4, 5, 6));
+    }
+
+    @Test
+    void case_2() {
+        ListNode sortedList = solution.mergeKLists(new ListNode[]{});
+
+        assertThat(sortedList).isNull();
+    }
+
+    @Test
+    void case_3() {
+        ListNode sortedList = solution.mergeKLists(new ListNode[]{null});
+
+        assertThat(sortedList).isNull();
+    }
 }
