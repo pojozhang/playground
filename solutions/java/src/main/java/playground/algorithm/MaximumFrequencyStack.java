259d8e3bb7d8f851533e6a19a846ab984fb5db74
Tony <pojo.zhang@gmail.com>
2021-04-04 15:49:49 +0800

    [java] add MaximumFrequencyStack

--- solutions/java/src/main/java/playground/algorithm/MaximumFrequencyStack.java
+++ solutions/java/src/main/java/playground/algorithm/MaximumFrequencyStack.java
@@ -1,2 +1,46 @@
-package playground.algorithm;public class MaximumFrequencyStack {
+package playground.algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class MaximumFrequencyStack {
+
+    private PriorityQueue<Entry> queue;
+    private Map<Integer, Integer> frequencies;
+    private int sequence = 0;
+
+    public MaximumFrequencyStack() {
+        this.frequencies = new HashMap<>();
+        this.queue = new PriorityQueue<>((o1, o2) -> {
+            if (o1.frequency == o2.frequency) {
+                return o2.sequence - o1.sequence;
+            }
+            return o2.frequency - o1.frequency;
+        });
+    }
+
+    public void push(int x) {
+        Integer frequency = frequencies.getOrDefault(x, 0) + 1;
+        this.queue.add(new Entry(x, frequency));
+        frequencies.put(x, frequency);
+    }
+
+    public int pop() {
+        Entry peek = this.queue.poll();
+        this.frequencies.put(peek.value, this.frequencies.get(peek.value) - 1);
+        return peek.value;
+    }
+
+    private class Entry {
+        int value;
+        int frequency;
+        int sequence;
+
+        public Entry(int value, int frequency) {
+            this.value = value;
+            this.frequency = frequency;
+            this.sequence = MaximumFrequencyStack.this.sequence++;
+        }
+    }
 }
--- solutions/java/src/test/java/playground/algorithm/MaximumFrequencyStackTest.java
+++ solutions/java/src/test/java/playground/algorithm/MaximumFrequencyStackTest.java
@@ -1,7 +1,24 @@
+package playground.algorithm;
 
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 class MaximumFrequencyStackTest {
-  
+
+    @Test
+    void case_1() {
+        MaximumFrequencyStack stack = new MaximumFrequencyStack();
+        stack.push(5);
+        stack.push(7);
+        stack.push(5);
+        stack.push(7);
+        stack.push(4);
+        stack.push(5);
+
+        assertThat(stack.pop()).isEqualTo(5);
+        assertThat(stack.pop()).isEqualTo(7);
+        assertThat(stack.pop()).isEqualTo(5);
+        assertThat(stack.pop()).isEqualTo(4);
+    }
 }
