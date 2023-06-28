c4bc77acf0e9b707e14cf92db0f6adc2398d63e7
Tony <pojo.zhang@gmail.com>
2023-06-29 00:40:51 +0800

    update BinarySearch

--- solutions/java/src/main/java/playground/algorithm/BinarySearch.java
+++ solutions/java/src/main/java/playground/algorithm/BinarySearch.java
@@ -3,21 +3,18 @@ package playground.algorithm;
 public class BinarySearch {
 
     public int search(int[] array, int target) {
-        return search(array, target, 0, array.length - 1);
-    }
-
-    private int search(int[] array, int target, int start, int end) {
-        if (start > end) {
-            return -1;
+        int left = 0, right = array.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (array[mid] == target) {
+                return mid;
+            }
+            if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-
-        int mid = (start + end) / 2;
-        if (array[mid] > target) {
-            return search(array, target, start, mid - 1);
-        }
-        if (array[mid] < target) {
-            return search(array, target, mid + 1, end);
-        }
-        return mid;
+        return -1;
     }
 }
